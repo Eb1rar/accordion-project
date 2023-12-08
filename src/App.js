ca7c89cacfe39f069bd2d153fc98102877fe52b6
ebrar <ebrkdmr1@gmail.com>
2023-12-08 14:15:16 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
 import data from "./data";
-import
+// import "./index.css";
+import SingleQuestion from "./Question";
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  //   const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {data.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default App;
